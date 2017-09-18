6d288f16ac03dfdcf340be7881e11e583d7214ef
reed <reedtrekks@gmail.com>
2017-09-18 11:54:02 -0500

    full

---
+++ README.md
@@ -0,0 +1,1 @@
+# Depot-U-Lab
---
+++ done.THISISJS.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".btn").on("click",function(){
+        alert("nav");
+    });
+});
+
+$(document).ready(function(){
+    $(".learnmore__button").on("click",function(){
+        alert("learn more");
+    });
+});
+
+
+$(document).ready(function(){
+    $(".footer__button").on("click",function(){
+        alert("more");
+    });
+});
